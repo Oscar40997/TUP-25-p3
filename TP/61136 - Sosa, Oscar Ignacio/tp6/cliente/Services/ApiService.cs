ceccff580ed59b2f1f9caf63e29e2fc5da19399e
Oscar40997 <Ignaciososa1ro@gmail.com>
2025-06-09 21:40:55 -0300

    renovando

--- TP/61136 - Sosa, Oscar Ignacio/tp6/cliente/Services/ApiService.cs
+++ TP/61136 - Sosa, Oscar Ignacio/tp6/cliente/Services/ApiService.cs
@@ -2,3 +2,26 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
+
+namespace Cliente.Services;
+
+public class ApiService
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILogger<ApiService> _logger;
+    private readonly string _endpoint;
+
+    public ApiService(HttpClient httpClient, ILogger<ApiService> logger, IConfiguration configuration)
+    {
+        _httpClient = httpClient;
+        _logger = logger;
+        _endpoint = configuration["ApiEndpoint"] ?? "/api/datos";
+    }
+
+    public async Task<DatosRespuesta> ObtenerDatosAsync()
+    {
+        
+    }
+
+
+}
